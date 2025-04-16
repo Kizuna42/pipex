d674b7f7ede32714af8e6137cc71e18cee65e6d6
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-04-16 16:42:02 +0900

    3

--- src/pipex_bonus_utils.c
+++ src/pipex_bonus_utils.c
@@ -6,7 +6,7 @@
 /*   By: kizuna <kizuna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 04:25:03 by kizuna            #+#    #+#             */
-/*   Updated: 2025/04/16 16:20:59 by kizuna           ###   ########.fr       */
+/*   Updated: 2025/04/16 16:40:25 by kizuna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,4 @@ int	is_limiter(char *line, char *limiter)
 	if (ft_strncmp(line, limiter, len) == 0 && line[len] == '\n')
 		return (1);
 	return (0);
-}
-
-void	close_all_pipes(t_pipex *pipex)
-{
-	int	i;
-
-	i = 0;
-	while (i < pipex->pipe_count)
-	{
-		close(pipex->pipes[i][0]);
-		close(pipex->pipes[i][1]);
-		i++;
-	}
-}
-
-void	wait_all_children(t_pipex *pipex)
-{
-	int	i;
-
-	i = 0;
-	while (i < pipex->cmd_count)
-	{
-		waitpid(pipex->pids[i], NULL, 0);
-		i++;
-	}
-	if (pipex->is_heredoc)
-		unlink(".heredoc_tmp");
 }
